d972dd29ed11282bab696950d69127eff1332446
Jakub Kropáček <kropikuba@gamil.com>
2022-05-20 08:45:40 +0200

    Maybe fixed migrations

--- migrations/env.py
+++ migrations/env.py
@@ -1,9 +1,7 @@
 from logging.config import fileConfig
 
-from sqlalchemy import create_engine, engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import create_engine, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -16,16 +14,18 @@ if alembic_config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from api.database import Base 
+from api.entities import Base  # noqa
+
 # target_metadata = mymodel.Base.metadata
-target_metadata = Base.metadata 
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-from api.config import DATABASE_URI
+from api.config import DATABASE_URI  # noqa
+
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -59,9 +59,7 @@ def run_migrations_online():
     """
     connectable = create_engine(DATABASE_URI, poolclass=pool.NullPool)
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
